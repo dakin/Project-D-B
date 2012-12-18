15fe6abcf40925ff8509d1bb88654d3e06d02ebd
Toby Schroder <toby.schroder@hudl.com>
2012-12-18 17:59:33 -0600

    more basement bits

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $('.team-menu').click(function () {
-    $('.basement-nav').show();
-    $('.subnav').animate({
-    		left: '92px'
-    });
-    $('.main-nav').animate({
-        left: '150px'
-    });
-    $('.main-nav').css('width', 200);
+    $('.basement-nav').toggle();
+    $('.main-nav').toggleClass('basement-show');
 });
