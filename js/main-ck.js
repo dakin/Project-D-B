35f14edee455da7652b136554110e6f6ba20b808
Toby Schroder <toby.schroder@hudl.com>
2012-12-18 23:55:45 -0600

    libby lu lu

--- js/main-ck.js
+++ js/main-ck.js
@@ -1,1 +1,1 @@
-$(".team-menu").click(function(){$(".basement-nav").toggleClass("show");$(".main-nav").toggleClass("basement-show")});
+$(".team-menu").click(function(){$(".basement-nav").toggleClass("show");$(".main-nav").toggleClass("basement-show")});$(".filterbar input").focus(function(){$(this).val("")});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $('.team-menu').click(function () {
-    $('.basement-nav').toggleClass('show');
-    $('.main-nav').toggleClass('basement-show');
+	$('.basement-nav').toggleClass('show');
+	$('.main-nav').toggleClass('basement-show');
+});
+
+$('.filterbar input').focus(function () {
+	$(this).val('');
 });
